6194d34b820a5b34553716161a1f1a4aa5227440
harshonline <122720163+harshonline@users.noreply.github.com>
2023-12-11 13:39:54 +0530

    start

--- hello.js
+++ hello.js
@@ -1,7 +1,7 @@
 var http=require('http')
 
 http.createServer(function(req, res){
-    res.writeHead(200, {'Content-Type':'text/html'})
+    res.writeHead(500, {'Content-Type':'text/html'})
     res.end("Welcome Back!")
     
 }).listen(8080)
