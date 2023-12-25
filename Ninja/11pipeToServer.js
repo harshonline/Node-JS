1a6d4c6237df159713f3a79c93d4c0a57e4ccb9a
harshonline <122720163+harshonline@users.noreply.github.com>
2023-12-25 19:44:45 +0530

    new pakage

--- Ninja/11pipeToServer.js
+++ Ninja/11pipeToServer.js
@@ -6,7 +6,7 @@ var fs = require('fs');
 var server = http.createServer(function(req, res){
     console.log('Hello World');
     res.writeHead(200, {'Content-Type':'text/html'});
-    var myReadStream= fs.createReadStream(__dirname + '/12html.html', 'utf-8'); //You can type any file name here and send it to server
+    var myReadStream = fs.createReadStream(__dirname + '/12html.html', 'utf-8'); //You can type any file name here and send it to server
     myReadStream.pipe(res);
 });
 
--- Ninja/14basicRouting.js
+++ Ninja/14basicRouting.js
@@ -1,14 +1,30 @@
+//This is routing based on using else if loops but not the express library
+
 var http = require('http');
 var fs = require('fs');
-const express = require('express')
-const app = express()
+const express = require('express');
+const { stringify } = require('querystring');
+const app = express();
 var server = http.createServer(function(req, res){
-    console.log('Listing to port 3000');
-    var home = 'You are in home page';
-    res.end(home);
-})
-
-
+    console.log('Request made');
+    if (req.url === '/' || req.url === '/home'){
+        res.writeHead(200, {'Content-Type' : 'text/html'});
+        fs.createReadStream(__dirname + '/12html.html').pipe(res);
+    }
+    else if (req.url === '/contact'){
+        res.writeHead(200, {'Content-Type' : 'text/html'});
+        fs.createReadStream(__dirname + '/contact.html').pipe(res);
+    }
+    else if (req.url === '/api/ninjas'){
+        var ninjas = [{name: 'ryu', age: 29}, {name: 'yoshi', age: 32}];
+        res.writeHead(200, {'Content-Type' : 'application/json'});
+        res.end(JSON.stringify(ninjas));
+    }
+    else{
+        res.writeHead(404, {'Content-Type':'text/html'});
+        fs.createReadStream(__dirname + '/z404.html').pipe(res);
+    }
+});
 
 server.listen(3000, '127.0.0.1');
-console.log('Hello World');
+console.log('Listning to port 3000');
